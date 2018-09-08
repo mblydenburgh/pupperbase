4d633737024665f951bc139ae557b424f05e65ad
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-09-08 15:35:00 -0400

    Third commit

--- app/app.js
+++ app/app.js
@@ -1,9 +1,3 @@
 (function(){
     var app = angular.module('pupperApp',[]);
-
-    // app.constant("config",{
-    //     apiKey: "5b91bfe419af4a22fafd4c52",
-    //     dbName: "awatoproject-4083",
-    //     collectionName: "dogs"
-    // });
 }());
