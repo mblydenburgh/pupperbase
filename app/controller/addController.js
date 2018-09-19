1527ebe99daded2c78d172b8091dcb47fad168d8
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-09-19 18:55:39 -0400

    added separate views for viewing and adding dogs

---
+++ app/controller/addController.js
@@ -0,0 +1,14 @@
+// (function(){
+//   var AddController = function ($scope,$http){
+
+//     $http({method:'GET', url:'https://awatoproject-4083.restdb.io/rest/dogs',headers:{
+//             apiKey:'5b91bfe419af4a22fafd4c52'}
+//         }.then()
+//   }
+  
+//   OrderController.$inject = ['$scope','$http'];
+
+//   angular.module('pupperApp')
+//   .controller("AddController", AddController);
+  
+// })
--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -1,5 +1,5 @@
 (function(){
-    var mainController = function ($scope,$http) {
+    var MainController = function ($scope,$http,$routeParams) {
 
         $http({method:'GET', url:'https://awatoproject-4083.restdb.io/rest/dogs',headers:{
             apiKey:'5b91bfe419af4a22fafd4c52'}
@@ -11,19 +11,13 @@
             console.log("$http Failure");
             $scope.data = "$http error"
         });
-        
-        /*$scope.data = [
-            {breed:"Akita",description:"The Akita is a large Spitz dog known for being protective and devoted to its family. Because of the Akita's protective nature it can show aggression towards other dogs and become suspicious of strangers. While a bit stubborn and independent, they are typically obedient towards their master and can be trained to behave around other dogs and strangers. Akitas require lots of exercise and prefer a home with a fenced backyard they can play in.",size:"Medium",lifespan:"10-12 Years"},
-            {breed: "Basset Hound",description: "Basset Hounds are friendly, loud, lovable dogs that are good with children and are relatively low-maintenance. Basset Hounds were originally bred to hunt small game and have excellent scenting skills. Despite their short legs they can move quickly so spending extra time on training or keeping them on a leash is essential.",size: "Medium",lifespan: "10-12 Years"},
-            {breed: "Pembroke Welsh Corgi",description: "Pembroke Welsh Corgis are natural herding dogs. As a result they may herd children by nipping at their heels while they play. These days they are most often seen as family dogs. Corgis are athletic and surprisingly fast. They can be a bit stubborn, have an independent streak and like to think for themselves.",size: "Medium",lifespan: "12-15 Years"}
-        ];*/
     
     }; //end mainController
 
-    mainController.$inject = ['$scope','$http'];
+    MainController.$inject = ['$scope','$http','$routeParams'];
 
-    angular.module('pupperApp',[])
-    .controller("mainController", mainController);
+    angular.module('pupperApp')
+    .controller("MainController", MainController);
 
 }());
 
