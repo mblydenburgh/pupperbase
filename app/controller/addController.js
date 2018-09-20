125f3078ecebb87d7d4165fa158fe3ce3c428cf6
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-09-20 17:23:02 -0400

    reorganize code

--- app/controller/addController.js
+++ app/controller/addController.js
@@ -1,23 +1,33 @@
 (function(){
     var AddController = function ($scope,$http,$routeParams) {
-        $scope.data = {breed:String,description:String,size:String,lifespan:String};
-
-        $scope.addDog = function(){
-            $http({method:'PUT',
+        $scope.form = {};
+        $scope.data = {}
+        $scope.addDog = function(dog){
+            $scope.data = angular.copy(dog)
+            $http({method:'POST',
                    url:'https://awatoproject-4083.restdb.io/rest/dogs',
-                   data: data,
+                   data: $scope.data,
                    headers:{apiKey:'5b91bfe419af4a22fafd4c52'}
         }).then(function success(response){
-            console.log('$http put success');
-            console.log(response);
+            //console.log('$http post success');
+            //console.log('data: '+$scope.dog);
+            //console.log('description: '+$scope.dog.description);
+            //console.log(response);
+            $scope.reset();
+            
         }, function failure(response){
-            console.log('$http put failure');
-            console.log(response);
+            //console.log('$http post failure');
+            //console.log(response);
         })
 
-        } //end add
 
+        } //end addDog
 
+        $scope.reset = function(){
+            $scope.dog = {};
+            $scope.form.dogForm.$setPristine(true);
+            console.log('reset!');
+        };
         
 
     
--- pupperbase-master/README.md
+++
@@ -1,7 +0,0 @@
-# pupperbase
-
-A simple application that utilizes AngularJS to connect to a RestDB database of dog information.
-
-Text box is used for searching through available breed names, and data can be sorted by the given headers.
-
-To come: update database with new entries based on form input
