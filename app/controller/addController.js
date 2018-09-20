2caac1e3fad05c1d4fdfc0e0e7367050c54a8be8
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-09-20 07:23:49 -0400

    attemping to get http post to work

--- app/controller/addController.js
+++ app/controller/addController.js
@@ -1,14 +1,31 @@
-// (function(){
-//   var AddController = function ($scope,$http){
+(function(){
+    var AddController = function ($scope,$http,$routeParams) {
+        $scope.data = {breed:String,description:String,size:String,lifespan:String};
 
-//     $http({method:'GET', url:'https://awatoproject-4083.restdb.io/rest/dogs',headers:{
-//             apiKey:'5b91bfe419af4a22fafd4c52'}
-//         }.then()
-//   }
-  
-//   OrderController.$inject = ['$scope','$http'];
+        $scope.addDog = function(){
+            $http({method:'PUT',
+                   url:'https://awatoproject-4083.restdb.io/rest/dogs',
+                   data: data,
+                   headers:{apiKey:'5b91bfe419af4a22fafd4c52'}
+        }).then(function success(response){
+            console.log('$http put success');
+            console.log(response);
+        }, function failure(response){
+            console.log('$http put failure');
+            console.log(response);
+        })
 
-//   angular.module('pupperApp')
-//   .controller("AddController", AddController);
-  
-// })
+        } //end add
+
+
+        
+
+    
+    }; //end mainController
+
+    AddController.$inject = ['$scope','$http','$routeParams'];
+
+    angular.module('pupperApp')
+    .controller("AddController", AddController);
+
+}());
